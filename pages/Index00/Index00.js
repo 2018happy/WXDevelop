838ce0d004b02f896383700e4caa479f51929482
KN2018 <5693386765@qq.com>
2019-02-22 13:46:11 +0800

    小程序本地存储学习笔记1

---
+++ pages/Index00/Index00.js
@@ -0,0 +1,76 @@
+// pages/Index00/Index00.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    //将键值对信息异步存储到手机中，永久存储
+   wx.setStorage({
+     key: 'Name',
+     data: [{'Kate':'666666'},{'Kate0':'999999'}],
+     success:function(){
+       console.log('Ok');
+     }
+   });
+   
+
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
